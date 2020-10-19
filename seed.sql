5f893b54bed03dea57fe053659a3b3eaec4fb49f
joey <joeywongsw@gmail.com>
2020-10-20 00:33:45 +0800

    updated seed.sql

---
+++ seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (first_name, last_name, username, password) VALUES ('Harry', 'Potter', 'hpotter', '004a00b3fac8202b02e70a2d9f1dd653c5866b');
+
+INSERT INTO items (name, quantity, purchase_date, expiry_date, description, user_id) VALUES ('Tomatoes', '5', '2020-10-18', '2020-10-25', '', '1');
+INSERT INTO items (name, quantity, purchase_date, expiry_date, description, user_id) VALUES ('Milk', '2 cartons', '2020-10-20', '2020-10-26', 'One chocolate, One Original', '1');
+INSERT INTO items (name, quantity, purchase_date, expiry_date, description, user_id) VALUES ('Tuna', '1 can', '2020-10-02', '2020-10-20', 'Chilli Tuna', '1');
