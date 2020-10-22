eea702c632a967f6f61ca73d3896bec3018749c4
joey <joeywongsw@gmail.com>
2020-10-23 01:24:35 +0800

    changed to signups.js

---
+++ client/src/components/authentication/Signups.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+
+function Signups(props) {
+
+    //////////////////////////////// HOOKS ///////////////////////////////////
+
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+
+    ////////////////////////////// FUNCTIONS /////////////////////////////////
+
+    // on change handlers
+    // sets the hooks with input values
+    const firstNameHandler = (e) => { setFirstName(e.target.value); setErrorMessage(""); }
+    const lastNameHandler = (e) => { setLastName(e.target.value); setErrorMessage(""); }
+    const usernameHandler = (e) => { setUsername(e.target.value); setErrorMessage(""); }
+    const passwordHandler = (e) => { setPassword(e.target.value); setErrorMessage(""); }
+
+    // on submit handler
+    // sends the request to the backend
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        try {
+            const body = { firstName, lastName, username, password }
+            const response = await fetch("/signup", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(body)
+            });
+
+            const verify = await response.json();
+            if (typeof (verify.result) === "string") {
+                setErrorMessage(verify.result);
+                setFirstName("");
+                setLastName("");
+                setUsername("");
+                setPassword("");
+            } else {
+                // props.history.push("/home");
+                window.location = "/items";
+            }
+        } catch (err) {
+            console.log("error at Signup submitHandler ===", err.message);
+        }
+    }
+
+    ///////////////////////////// SIGNUP FORM  ///////////////////////////////
+
+    return (
+        <div>
+            <form onSubmit={submitHandler}>
+                First Name: <input type='text' value={firstName} required onChange={firstNameHandler} />
+                <br />
+                Last Name: <input type='text' value={lastName} required onChange={lastNameHandler} />
+                <br />
+                Username: <input type='text' value={username} minLength="6" required onChange={usernameHandler} />
+                <br />
+                Password: <input type='password' value={password} minLength="8" required onChange={passwordHandler} />
+                <br />
+                <input type="submit" value="Sign Up" />
+            </form>
+            {errorMessage ? <h5>{errorMessage}</h5> : null}
+        </div>
+    )
+}
+
+export default Signups
